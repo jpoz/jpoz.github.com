6a52a95556cbe17738cd9a52aa27e52d78ab9b76
James Taco Pozdena <jpoz@simple.com>
2013-02-19 00:31:53 -0800

    boom

---
+++ source/blog/2009-01-30-hash_to_url_params.html.markdown
@@ -0,0 +1,16 @@
+---
+  title: Hash.to_url_params
+  date: January 30th, 2009
+---
+
+Google couldn’t find me a good one. So I made this one:
+
+<script src="https://gist.github.com/jpoz/4975829.js"></script>
+
+Will transform:
+
+<script src="https://gist.github.com/jpoz/1c13878aa8b63be24421.js"></script>
+
+Into:
+
+<script src="https://gist.github.com/jpoz/179e83b6d4ab8a6bc9c8.js"></script>
---
+++ source/javascripts/app.js
@@ -0,0 +1,56 @@
+var PageContent = new Class({
+
+  initialize: function(el) {
+    this.element = el;
+    this.element.addEvent('click', this.click.bind(this));
+    this.element.addEvent('touch', this.click.bind(this));
+  },
+
+  click: function() {
+    $('body').removeClass('show_nav');
+  }
+
+})
+
+var NavButton = new Class({
+
+  initialize: function(el) {
+    this.element = el;
+    console.log(el);
+    this.element.addEvent('click', this.click.bind(this));
+    this.element.addEvent('touch', this.click.bind(this));
+  },
+
+  click: function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('body').toggleClass('show_nav');
+  }
+
+});
+
+var TextFader = new Class({
+  initialize: function(el) {
+    this.element = el
+    this.show();
+  },
+
+  show: function() {
+    this.orig = this.element.get('text');
+    var sentences = this.orig.split('').map(function(sentence) {
+      var s = new Element('span', {'html':sentence + ""});
+      s.set('tween', {duration: 'long'});
+      return s;
+    });
+    this.element.set('html', "");
+    this.element.adopt(sentences);
+
+    sentences.each(function(sentence, index) {
+      setTimeout(function() {
+        var o = 0.5 - 0.001 * index;
+        sentence.tween('opacity', o)
+      }, index * 37);
+    });
+  }
+
+})
