35981d366c87aa328f7578afcd8a0db9dd0aca8c
James Taco Pozdena <jpoz@simple.com>
2013-05-06 23:01:08 -0700

    updating

--- config.rb
+++ config.rb
@@ -29,6 +29,8 @@
 # end
 
 # Proxy (fake) files
+proxy "/blog", "index.html"
+
 # page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
 #   @which_fake_page = "Rendering a fake page with a variable"
 # end
---
+++ source/blog/2013-04-28-Why_Go_Is_cool.html.markdown
@@ -0,0 +1,8 @@
+---
+title: Why I like Go
+
+---
+
+I like Go because it keeps me busy and rarely annoyed.
+
+
