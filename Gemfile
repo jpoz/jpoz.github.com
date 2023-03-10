285d4e5c56148d87ddca61219e4f9f3f4d96cc62
James Pozdena <jpozdena@gmail.com>
2023-03-10 09:04:15 -0800

    update the ol' resume

--- Gemfile
+++ Gemfile
@@ -1,8 +1,5 @@
 source 'https://rubygems.org'
 
-gem 'middleman', '~> 5.0.0.rc.1'
-gem 'middleman-autoprefixer', '~> 2.10'
-gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby, :x64_mingw]
-gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw, :x64_mingw]
+gem 'middleman'
 
 gem 'rake'
--- config.rb
+++ config.rb
@@ -1,9 +1,9 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
-activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
-end
+# activate :autoprefixer do |prefix|
+#   prefix.browsers = "last 2 versions"
+# end
 
 # Layouts
 # https://middlemanapp.com/basics/layouts/
